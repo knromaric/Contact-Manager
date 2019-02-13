3bdad8f81fae1a3911da099b69c8ddd408b92c63
knromaric <nzekeng@gmail.com>
2019-02-13 23:32:35 +0200

    implemented the events onShowContactDetails

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,38 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-class Contact extends Component {
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-  render() { 
-    const {name, email, phone} = this.props.contact;
-    return ( 
+class Contact extends Component {
+  state = {
+    showContactDetails: false
+  };
+
+  onShowContactDetails = e => {
+    this.setState({ showContactDetails: !this.state.showContactDetails });
+  };
+
+  render() {
+    const { name, email, phone } = this.props.contact;
+    const { showContactDetails } = this.state;
+
+    return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{" "}
+          <i onClick={this.onShowContactDetails} className="fas fa-sort-down" />
+        </h4>
+        {showContactDetails ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
-     );
+    );
   }
 }
 
-Contact.propTypes={
-  contact: PropTypes.object.isRequired,
-}
- 
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
 export default Contact;
