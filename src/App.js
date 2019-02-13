9cba0e3d61465836e41a14c937be4d6f36c91895
knromaric <nzekeng@gmail.com>
2019-02-13 22:28:14 +0200

    Initial commit: project structure + basic Contact and Header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact 
+            name="Roma Nze" 
+            email="rnze@gmail.com" 
+            phone="777-777-777" />
+          <Contact 
+            name="Nina Koua" 
+            email="ninkoua@gmail.com" 
+            phone="333-333-333" />
+        </div>
       </div>
     );
   }
