9c1c21ef3aaea56ec4f8204f9edfd6a422140aad
knromaric <nzekeng@gmail.com>
2019-02-16 21:22:47 +0200

    Updates

---
+++ precache-manifest.8d0cccb157be078e4d60a8f3baa9b24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b3e93421dd7ff864081",
+    "url": "/Contact-Manager/static/js/runtime~main.3b3e9342.js"
+  },
+  {
+    "revision": "0b1365367915483aba52",
+    "url": "/Contact-Manager/static/js/main.0b136536.chunk.js"
+  },
+  {
+    "revision": "dfb1fb08cd637840dc3c",
+    "url": "/Contact-Manager/static/js/2.dfb1fb08.chunk.js"
+  },
+  {
+    "revision": "0b1365367915483aba52",
+    "url": "/Contact-Manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "dfb1fb08cd637840dc3c",
+    "url": "/Contact-Manager/static/css/2.297bc334.chunk.css"
+  },
+  {
+    "revision": "3c0f8204218ccbeeebbbee5c3b3437e6",
+    "url": "/Contact-Manager/index.html"
+  }
+];
